cc9fe3afb3c1ada6190def7dd6a74fe1be590684
Artemii Strukov <kavrese@gmail.com>
2026-02-02 21:31:25 +0300

    added images and icons

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,3 @@ import 'package:mood_diary_evo_test/presentation/app.dart';
 void main() {
   runApp(const MyApp());
 }
-
-
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
